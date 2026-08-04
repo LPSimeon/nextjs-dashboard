a70ee8edf8fa4f93232c64d633007eae6e88d22d
LP Simeon <lp.simeon@yahoo.com>
2026-08-04 19:14:47 +0200

    feat: Added CustomersTable component in customers Page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,14 @@
 import { Metadata } from "next";
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import CustomersTable from "@/app/ui/customers/table";
 
 export const metadata: Metadata = {
   title: "Customers",
 };
 
-export default function Page() {
-  return <p>Customer Page</p>;
+export default async function Page() {
+  const customers = await fetchFilteredCustomers("");
+  // console.log(customers);
+
+  return <CustomersTable customers={customers}></CustomersTable>;
 }
